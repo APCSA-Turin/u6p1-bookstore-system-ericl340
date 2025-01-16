e48c80d659d672e909520324ac75d718aefff521
ericl340 <ericl340@nycstudents.net>
2025-01-16 13:59:03 +0000

    u

--- src/main/java/com/example/project/BookStore.java
+++ src/main/java/com/example/project/BookStore.java
@@ -1,11 +1,9 @@
 package com.example.project;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
 public class BookStore{
 
     //requires at least 2 attributes Book[] books, User[] users (initialized to an empty array of 10 max users) 
-    private Book[] books = new Book[5];
+    private Book[] books = new Book[0];
     private User[] users = new User[10];
     //requires 1 empty constructor
     public BookStore() {}
@@ -53,44 +51,60 @@ public class BookStore{
         users = nUsers;
     }
     public void consolidateBooks(){
-        Book[] nBooks = new Book[books.length];
-        int i = 0;
+        int l = 0;
         for (Book b: books) {
             if (b != null) {
-                nBooks[i] = b;
+                l++;
+            }
+        }
+        Book[] n = books;
+        books = new Book[l];
+        int i = 0;
+        for (Book b: n) {
+            if (b != null) {
+                books[i] = b;
                 i++;
             }
         }
-        books = nBooks;
     }
     public void addBook(Book book){
-        for (int i = 0; i < books.length; i++) {
-            if (books[i] == null) {
-                books[i] = book;
-                break;
-            }
+        Book[] p = books;
+        books = new Book[books.length + 1];
+        for (int i = 0; i < p.length; i++) {
+            books[i] = p[i];
         }
+        books[p.length] = book;
     }
 
     public void insertBook(Book book, int index){
-        Book[] copy = new Book[books.length];
-        for (int i = 0; i < books.length; i++) {
-            copy[i] = books[i];
+        Book[] c = books;
+        books = new Book[0];
+        for (int i = 0; i < c.length; i++) {
+            if (i == index) {
+                addBook(book);
+            }
+            addBook(c[i]);
         }
-        books = new Book[books.length];
-        books[index] = book;
-        addBook(book);
+        if (index == c.length) {
+            addBook(book);
+        }
     }
 
     public void removeBook(Book book){
         String isbn = book.getIsbn();
-        for (int i = 0; i < users.length; i++) {
-            if (books[i] != null && books[i].getIsbn().equals(isbn)) {
-                books[i] = null;
+        int q;
+        for (int i = 0; i < books.length; i++) {
+            if (books[i].getIsbn().equals(isbn)) {
+                q = books[i].getQuantity();
+                if (q > 1) {
+                    books[i].setQuantity(q - 1);
+                }else{
+                    books[i] = null;
+                    consolidateBooks();
+                }
                 break;
             }
         }
-        consolidateBooks();
     }
        
     public String bookStoreBookInfo(){
@@ -108,16 +122,4 @@ public class BookStore{
         }
         return res;
     } //you are not tested on this method but use it for debugging purposes
-    public static void main(String[] args) {
-                BookStore store = new BookStore(); 
-        Book b1 = new Book("The Great Gatsby","Scott Fitzgerald", 1925, "979-8351145013",3);
-        Book b2 = new Book("The Outliers", "Malcolm Gladwell",2008,"978-0316017930",1);
-        Book b3 = new Book("1984", "George Orwell", 1949, "978-0451524935", 5);
-        Book b4 = new Book("Brave New World", "Aldous Huxley", 1932, "978-0060850524", 3);
-        Book b5 = new Book("Test","Author",1900, "1234", 1);
-        store.addBook(b1);store.addBook(b2);store.addBook(b3);store.addBook(b4);store.addBook(b5);
-        Book[] expected = {b1,b2,b3,b4,b5};
-        System.out.println(store.bookStoreBookInfo());
-    }
-
 }
